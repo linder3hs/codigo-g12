7cd285bea94c2f6dfaf1b8ee67e2dc2ef19cca76
linder3hs <linderhassinger00@gmail.com>
2023-02-22 23:09:24 -0500

    chore: add js slider

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const challenges = document.querySelector(".challenges");
+const btnNext = document.querySelector(".btn-next");
+const btnBack = document.querySelector(".btn-back");
 
-challenges.addEventListener("wheel", function () {
+btnNext.onclick = function () {
   challenges.scrollBy({
     left: 50,
   });
-});
+};
+
+btnBack.onclick = function () {
+  challenges.scrollBy({
+    left: -50,
+  });
+};
