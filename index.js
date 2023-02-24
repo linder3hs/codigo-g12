d424839afe420d8856fc64f473e69cb94ecf5d77
linder3hs <linderhassinger00@gmail.com>
2023-02-23 20:08:14 -0500

    chore: add slider

--- index.js
+++ index.js
@@ -1,15 +1,25 @@
+// Esta es formar en la que JS accede a los elementos
+
+// es el div donde estan todos nuestros cards
+// recordemos que el div con la clase challenge tiene el scroll x (horizontal)
 const challenges = document.querySelector(".challenges");
+
+// obtener los botones de next y back
 const btnNext = document.querySelector(".btn-next");
 const btnBack = document.querySelector(".btn-back");
 
 btnNext.onclick = function () {
+  // Esto quiere decir que cuando hagamos un click en el boton con clase .btn-bnext 
+  // hara lo siguiente
+
+  // challenges.offsetWidth => nos da el ancho del div challenges
   challenges.scrollBy({
-    left: 50,
+    left: challenges.offsetWidth / 4,
   });
 };
 
 btnBack.onclick = function () {
   challenges.scrollBy({
-    left: -50,
+    left: (challenges.offsetWidth / 4) * -1,
   });
 };
