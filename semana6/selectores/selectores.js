a488e4114be0656823d8cd0f7d862d33dd6c10f2
linder3hs <linderhassinger00@gmail.com>
2023-03-09 19:56:22 -0500

    feat: challenge 6

--- semana6/selectores/selectores.js
+++ semana6/selectores/selectores.js
@@ -13,3 +13,26 @@ const segunBoton = document.getElementsByClassName("segundo-boton");
 
 console.log("BY CLASS");
 console.log(segunBoton);
+
+console.log("USANDO LA FORMA ACTUAL");
+
+// class = .
+// id = #
+// querySelector => retorna solo 1 elemento
+// querySelectorAll => retorna un array de elemento
+
+const box = document.querySelector(".box");
+
+console.log("box", box);
+
+const boxs = document.querySelectorAll(".box");
+
+console.log("boxs", boxs);
+
+const profile = document.querySelector("#profile");
+
+console.log("image", profile);
+
+const tituloMediano = document.querySelectorAll("h5");
+
+console.log(tituloMediano[1]);
