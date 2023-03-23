2a4999b3935eb204e8b20ec23f9446867eb63cb1
linder3hs <linderhassinger00@gmail.com>
2023-03-22 20:07:41 -0500

    reto 11

--- semana7/reto11/index.js
+++ semana7/reto11/index.js
@@ -4,13 +4,43 @@ const tasks = document.querySelector(".tasks");
 
 const apiURL = "https://641523a24f32ca32918f7b1f.mockapi.io/tasks";
 
+async function deleteTask(element) {
+  const id = element.dataset.id;
+
+  const response = await fetch(`${apiURL}/${id}`, {
+    method: "DELETE",
+    headers: {
+      "content-type": "application/json",
+    },
+  });
+
+  if (!response.ok) {
+    alert("algo salio mal");
+    return;
+  }
+
+  console.log(response);
+}
+
 // crear una funcion que liste (get) las tareas
 async function getTasks() {
   const response = await fetch(apiURL);
   const data = await response.json();
 
   data.forEach((task) => {
-    tasks.innerHTML += `<h2>${task.name}</h2>`;
+    tasks.innerHTML += `
+      <div class="card mt-3">
+        <div class="card-body">
+          <div>
+            <h4>${task.name}</h4>
+          </div>
+          <div>
+            <button class="btn btn-primary">Terminado</button>
+            <button class="btn btn-warning">Editar</button>
+            <button onclick="deleteTask(this)" data-id="${task.id}" class="btn btn-danger">Eliminar</button>
+          </div>
+        </div>
+      </div>`;
   });
 }
 
@@ -35,6 +65,6 @@ form.onsubmit = async function (event) {
   const data = await response.json();
 
   tasks.innerHTML += `<h2>${data.name}</h2>`;
-  
+
   console.log(data);
 };
