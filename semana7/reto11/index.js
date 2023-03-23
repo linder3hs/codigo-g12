eb4f1bedbe5dc22af71cd5b0aa4de38ba805880a
linder3hs <linderhassinger00@gmail.com>
2023-03-22 22:04:23 -0500

    👨🏻‍💻 chore: reto11

--- semana7/reto11/index.js
+++ semana7/reto11/index.js
@@ -43,16 +43,11 @@ async function updateTask(id) {
   }
 
   const textTask = document.querySelector(`#name-${id}`);
-  textTask.textContent = newText; 
+  textTask.textContent = newText;
 }
 
-// crear una funcion que liste (get) las tareas
-async function getTasks() {
-  const response = await fetch(apiURL);
-  const data = await response.json();
-
-  data.forEach((task) => {
-    tasks.innerHTML += `
+function renderTask(task) {
+  return `
       <div class="card mt-3">
         <div class="card-body">
           <div>
@@ -65,6 +60,15 @@ async function getTasks() {
           </div>
         </div>
       </div>`;
+}
+
+// crear una funcion que liste (get) las tareas
+async function getTasks() {
+  const response = await fetch(apiURL);
+  const data = await response.json();
+
+  data.forEach((task) => {
+    tasks.innerHTML += renderTask(task);
   });
 }
 
@@ -88,18 +92,7 @@ form.onsubmit = async function (event) {
   });
   const data = await response.json();
 
-  tasks.innerHTML += `<div class="card mt-3">
-        <div class="card-body">
-          <div>
-            <h4>${data.name}</h4>
-          </div>
-          <div>
-            <button class="btn btn-primary">Terminado</button>
-            <button class="btn btn-warning">Editar</button>
-            <button onclick="deleteTask(this)" data-id="${data.id}" class="btn btn-danger">Eliminar</button>
-          </div>
-        </div>
-      </div>`;
+  tasks.innerHTML += renderTask(data);
 
   console.log(data);
 };
